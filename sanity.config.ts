51399f8216e5a4fae781a076362806e260f632bd
muhammad-hashir-butt <hashirbutt303@gmail.com>
2026-05-20 16:06:00 +0500

    base bath added

--- sanity.config.ts
+++ sanity.config.ts
@@ -14,6 +14,7 @@ import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
 
 export default defineConfig({
+  basePath: "admin",
   projectId: '2sc5lcun',
   dataset: 'production',
   // Add and edit the content schema in the './sanity/schemaTypes' folder
